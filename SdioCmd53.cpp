b66497c6b3646736537145bcb37e5f4407e6a4ca
Jonathan Conway <jonco29@gmail.com>
2014-08-08 16:32:53 -0500

    added dynamic cast example and input file

--- SdioCmd53.cpp
+++ SdioCmd53.cpp
@@ -1,14 +1,61 @@
 #include "SdioCmd53.h"
 #include "SDIOParser.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
 
 const char * SdioCmd53::getShortString()
 {
-    return "short string";
+    ostringstream stream;
+    char format[200] = {0};
+
+    stream << "0x" << hex << cmdData << " CMD53 ";
+    if (getRead()) stream << "R:";
+    else stream << "W:";
+
+    stream << getXferCount();
+
+    if (isBlockMode()) stream << " Blks";
+    else stream << " Bytes";
+
+    string str = stream.str();
+    const char * chr = str.c_str();
+
+    return chr;
 }
 
 const char * SdioCmd53::getDetailedString()
 {
-    return "detailed string";
+    ostringstream stream;
+    char format[200] = {0};
+
+    stream << hex << cmdData << " CMD53 ";
+    stream << "Function: " << getFunctionNumber() << ", ";
+    if (getRead()) stream << "Read, ";
+    else stream << "Write, ";
+
+
+    stream << getXferCount();
+
+    if (isBlockMode()) stream << " Blocks, ";
+    else stream << " Bytes, ";
+
+    stream << "From Address: " << hex << getRegisterAddress() << ", ";
+
+    if (isIncrementingAddress()) stream << "using Incrementing Addresses";
+    else stream << "using Fixed Addressing";
+
+    string str = stream.str();
+    const char * chr = str.c_str();
+
+    return chr;
 }
 bool SdioCmd53::getRead()
 {
--- tester.cpp
+++ tester.cpp
@@ -1,15 +1,53 @@
-#include <iostream> 
 #include "SdioCmd53.h"
+#include <iostream> 
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 using namespace std;
 
 
+void canned();
+void process(U64 data);
+void processCmd53(U64 data);
 int main (int argc, char** argv)
+{
+    U64 input;
+    if(argc <= 1)
+    {
+        canned();
+    }
+    else
+    {
+       ifstream inStream(argv[1], fstream::in);
+
+        if (inStream.is_open())
+        {
+            while (!inStream.eof())
+            {
+                inStream >> uppercase >> hex >> input;
+                //cout << "jonathan " << uppercase << hex << input << " " << input - 1 <<endl;
+                cout << hex << input << "  -- " << endl;
+                //cout << parse_str(input) << endl;
+                process(input);
+            }
+
+            inStream.close();
+        }
+    }
+    return 0;
+}
+
+void canned()
 {
     U64 data = 0x7514000068F9;
     SdioCmd *cmd = new SdioCmd53(data);
 
     cout << "short string: " << cmd->getShortString() << endl;
-    cout << "short string: " << cmd->getDetailedString() << endl;
+    cout << "detailed string: " << cmd->getDetailedString() << endl;
     cout << "CMD: " << hex << cmd->getCmd() << endl;;
     cout << "DIR: " << hex << cmd->getDir() << endl;;
 
@@ -17,15 +55,45 @@ int main (int argc, char** argv)
     cout << "Write: " << ((SdioCmd53*)cmd)->getWrite() << endl;
 
     cout << "Function Number: " << ((SdioCmd53*)cmd)->getFunctionNumber() << endl;
-    // bool isBlockMode();
-    // bool isByteMode();
-    // bool isFixedAddress();
-    // bool isIncrementingAddress();
     cout << "BlockMode: " << ((SdioCmd53*)cmd)->isBlockMode() << endl;
     cout << "ByteMode: " << ((SdioCmd53*)cmd)->isByteMode() << endl;
     cout << "Fixed Address: " << ((SdioCmd53*)cmd)->isFixedAddress() << endl;
     cout << "Incrementing Address: " << ((SdioCmd53*)cmd)->isIncrementingAddress() << endl;
     cout << "Register Address: 0x" << hex << ((SdioCmd53*)cmd)->getRegisterAddress() << endl;
     cout << "Transfer Count (byte/block): " << ((SdioCmd53*)cmd)->getXferCount() << endl;
+
+}
+void process(U64 data)
+{
+    U32 cmd = CMD_VAL(data);
+
+    switch (cmd)
+    {
+        case 53:
+            processCmd53(data);
+            break;
+        default:
+            break;
+    }
+}
+void processCmd53(U64 data)
+{
+    SdioCmd53 *cmd = new SdioCmd53(data);
+
+    cout << "short string: " << cmd->getShortString() << endl;
+    cout << "detailed string: " << cmd->getDetailedString() << endl;
+    // cout << "CMD: " << hex << cmd->getCmd() << endl;;
+    // cout << "DIR: " << hex << cmd->getDir() << endl;;
+
+    // cout << "Read: " << cmd->getRead() << endl;
+    // cout << "Write: " << cmd->getWrite() << endl;
+
+    // cout << "Function Number: " << cmd->getFunctionNumber() << endl;
+    // cout << "BlockMode: " << cmd->isBlockMode() << endl;
+    // cout << "ByteMode: " << cmd->isByteMode() << endl;
+    // cout << "Fixed Address: " << cmd->isFixedAddress() << endl;
+    // cout << "Incrementing Address: " << cmd->isIncrementingAddress() << endl;
+    // cout << "Register Address: 0x" << hex << cmd->getRegisterAddress() << endl;
+    // cout << "Transfer Count (byte/block): " << cmd->getXferCount() << endl;
 
 }
